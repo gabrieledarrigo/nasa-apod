c33bcecf95b8adb6cb68db9410659d7fa098a05d
Gabriele D'Arrigo <gdarrigo@ebay.com>
2016-02-06 13:30:04 +0100

    [NASA_APOD] Refactoring 	- Break styles in various components. 	- React day picker component. 	- More generic React component's design.

--- src/js/components/Picture.js
+++ src/js/components/Picture.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
 export const Picture = React.createClass({
-	render() {
-		return (
-			<div className="content">
-				<figure className="picture">
-					<h4 className="title">
-						{this.props.data.title}
-					</h4>
-					
-					<div className="picture__content">
-						<img src={this.props.data.url} title={this.props.data.title} className="picture__img" />
-					</div>
-				
-					<h5 className="description">
-						{this.props.data.explanation}
-					</h5>
-				</figure>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="content">
+                <figure className="picture">
+                    <div className="picture__content">
+                        <img src={this.props.data.url} title={this.props.data.title} className="picture__img" />
+                    </div>
+
+                    <figcaption>
+                        <h4 className="title">
+                            {this.props.data.title}
+                        </h4>
+
+                        <h5 className="description">
+                            {this.props.data.explanation}
+                        </h5>
+                    </figcaption>
+                </figure>
+            </div>
+        );
+    }
 });
